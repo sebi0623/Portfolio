9c8af4ec5a77e6979434c46a401ea5da2ced7bc7
sebi0623 <30859470+sebi0623@users.noreply.github.com>
2024-01-30 23:14:52 +0000

    Portfolio site Skeleton

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express ();
+const port = process.env.PORT || 3000; //Use specified port or default to 3000
+
+//Static Middleware
+//serve static files from public directory
+app.use(express.static('client'));
+
+//serve react build files
+app.use(express.static('client/build'));
+
+//Route to homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+//Start server test for the server working
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+})
